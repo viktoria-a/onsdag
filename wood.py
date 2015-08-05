51dded30c55f0b7afa8dd2afa54130bb586b42a4
viktoria-a <vianlo@Viktorias-MacBook-Pro.local>
2015-08-05 11:32:46 +0200

    onsdagens tester

---
+++ wood.py
@@ -0,0 +1,22 @@
+#coding: utf-8
+#wood_price= 900
+#wood_count= 2
+#wood_vat= 1.25
+
+#book_price= 100
+#book_count=1
+#book_vat=1.06
+
+#print wood_price * wood_count * wood_vat + book_price * book_count * book_vat
+
+#wood_companies_phone_number = ["070444388282", "0877839382", "0847390484"]
+#print wood_companies_phone_number[2]
+
+#wood_companies_by_name= { "Norrlands skog": "070444388282", 
+#"Svea skog": "0877839382",
+#u"Göta skog": "0847390484" }
+#print wood_companies_by_name[u"Göta skog"]
+
+wood= {"price": 900, "count": 2, "vat": 1.25}
+book= {"price": 100, "count": 1, "vat": 1.06}
+print wood ["price"] * wood ["vat"] + book ["price"] * book ["vat"] 
